48149b379a8966273fdd6f02451e6b750a2dd58c
DanielJamesF <fredericksd17@gmail.com>
2022-10-10 08:38:12 +0200

    changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -118,63 +118,12 @@ ul {
     z-index: 2;
 }
 
-.footer-pattern img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 330px;
-    background-size: cover;
-    background-position: 100% 100%;
-}
-
 .footer-logo {
     margin-bottom: 30px;
 }
 
 .footer-logo img {
     max-width: 200px;
-}
-
-.footer-text p {
-    margin-bottom: 14px;
-    font-size: 14px;
-    color: #7e7e7e;
-    line-height: 28px;
-}
-
-.footer-social-icon span {
-    color: #fff;
-    display: block;
-    font-size: 20px;
-    font-weight: 700;
-    font-family: 'Poppins', sans-serif;
-    margin-bottom: 20px;
-}
-
-.footer-social-icon a {
-    color: #fff;
-    font-size: 16px;
-    margin-right: 15px;
-}
-
-.footer-social-icon i {
-    height: 40px;
-    width: 40px;
-    text-align: center;
-    line-height: 38px;
-    border-radius: 50%;
-}
-
-.facebook-bg {
-    background: #3B5998;
-}
-
-.twitter-bg {
-    background: #55ACEE;
-}
-
-.google-bg {
-    background: #DD4B39;
 }
 
 .footer-widget-heading h3 {
@@ -224,19 +173,5 @@ ul {
 
 .copyright-text p a {
     color: white;
-}
-
-.footer-menu li {
-    display: inline-block;
-    margin-left: 20px;
-}
-
-.footer-menu li:hover a {
-    color: white;
-}
-
-.footer-menu li a {
-    font-size: 14px;
-    color: #878787;
 }
 </style>
