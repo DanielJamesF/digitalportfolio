7c8f229c2d359259ca9af219e792c09336765dc7
DanielJamesF <fredericksd17@gmail.com>
2023-01-17 09:48:42 +0200

    changes

--- src/views/Testimonials.vue
+++ src/views/Testimonials.vue
@@ -20,32 +20,35 @@
             <div class="carousel-item active">
               <img src="https://i.postimg.cc/3NbhYdM2/Joel.jpg" style="object-fit: cover" alt="Joel" id="image"
                 class="rounded-circle" />
-              <h5>Joel Mukanya</h5>
+              <h4>Joel Mukanya</h4>
               <div id="comments">
                 <p>
                   Daniel is respectful and takes his studies seriously. Daniel has
                   demonstrated a key grasp of the Web Design topics we have
                   covered in class. He continues to be an excellent student. Your
-                  organization would benefit from his skills. Joel Mukanya- Coding
-                  Lecturer.
+                  organization would benefit from his skills.
+                </p>
+                <p>
+                  - Coding Lecturer.
                 </p>
               </div>
             </div>
             <div class="carousel-item">
               <img src="https://i.postimg.cc/HpHW15CY/Clayton.jpg" style="object-fit: cover" alt="Clayton" id="image"
                 class="rounded-circle" />
-              <h5>Clayton Adonis</h5>
+              <h4>Clayton Adonis</h4>
               <div id="comments">
                 <p>
                   Daniel is an absolute delight to work with and he always makes
                   any room he is in better.
                 </p>
+                <p>- Colleague</p>
               </div>
             </div>
             <div class="carousel-item">
               <img src="https://i.postimg.cc/cLWvzbmZ/Muddathir.jpg" style="object-fit: cover" alt="Muddathir"
                 id="image" class="rounded-circle" />
-              <h5>Muddathir Dawood</h5>
+              <h4>Muddathir Dawood</h4>
               <div id="comments">
                 <p>
                   Daniel is a developer with potential to become something great
@@ -53,12 +56,13 @@
                   each project he is given and motivates me and other to do
                   better. He will add an enormous amount of value to any team
                 </p>
+                <p>- Colleague</p>
               </div>
             </div>
             <div class="carousel-item">
               <img src="https://i.postimg.cc/Fspv9Cvh/Jared.jpg" style="object-fit: cover" alt="Jared" id="image"
                 class="rounded-circle" />
-              <h5>Jared Isaacs</h5>
+              <h4>Jared Isaacs</h4>
               <div id="comments">
                 <p>
                   Daniel is an outgoing, hardworking, creative web developer. Who
@@ -69,18 +73,24 @@
                   set skills to fulfill any task that will be given to him, making
                   him a great asset to any business or company.
                 </p>
+                <p>
+                  - Colleague
+                </p>
               </div>
             </div>
             <div class="carousel-item">
               <img src="https://i.postimg.cc/kgSyQz5m/Reagan1.jpg" style="object-fit: cover" alt="Reagan" id="image"
                 class="rounded-circle" />
-              <h5>Reagan Carolussen</h5>
+              <h4>Reagan Carolussen</h4>
               <div id="comments">
                 <p>
                   Daniel is a trustworthy, multi-talented individual that shows
                   the ability to learn and grasp new concepts quickly. He is
                   focused and works will with others. I highly recommend him to
                   any team or organization.
+                </p>
+                <p>
+                  - Colleague
                 </p>
               </div>
             </div>
