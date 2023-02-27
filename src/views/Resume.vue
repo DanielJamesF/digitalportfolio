7f4d2bba64980c281bb11151fe32ee6e41d1a35f
DanielJamesF <fredericksd17@gmail.com>
2023-02-27 14:00:42 +0200

    removed unecessary code added work experience

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,145 +1,189 @@
 <template>
-<section id="section">
-    <div class="row" id="head">
-        <div class="col">
-            <h2>Resume</h2>
-        </div>
-    </div>
-    <div class="container">
-        <h3 class="py-3">Education :</h3>
-        <div class="row" id="education">
+    <section id="section">
+        <div class="row" id="head">
             <div class="col">
-                <div class="row p-2">
-                    <div class="col-4 col-md-4">
-                        <p>March 2022 - August 2022</p>
-                    </div>
-                    <div class="col-2 col-md-1">
-                        <span>-</span>
-                    </div>
-                    <div class="col-6 col-md-7">
-                        <p>
-                            I am attendiing Life Choices Academy where I am learning to
-                            code, where we recently covered HTML, CSS, Bootstrap, mySQL,
-                            JavaScript and Vuex
-                        </p>
-                    </div>
+                <h2>Resume</h2>
+            </div>
+        </div>
+        <div class="container">
+            <h3 class="py-3">Education :</h3>
+            <div class="row p-2" id="education">
+                <div class="col-4 col-md-4">
+                    <p>March 2022 - August 2022</p>
                 </div>
-                <div class="row p-2">
-                    <div class="col-4 col-md-4">
-                        <p>January 2015 - December 2019</p>
-                    </div>
-                    <div class="col-2 col-md-1">
-                        <span>-</span>
-                    </div>
-                    <div class="col-6 col-md-7">
-                        I attended Grassy Park high School and got my NQ4 form Grassy Park
-                        High School from which I matriculated with a Diploma pass
+                <div class="col-2 col-md-1">
+                    <span>-</span>
+                </div>
+                <div class="col-6 col-md-7">
+                    <p>
+                        I attended a six Month Full Stack Web Development Bootcamp at Life Choices Coding Academy where I
+                        learned how to create Fullstack Websites and Web apps that were are both fully dunctional and mobile
+                        responsivee
+                    </p>
+                </div>
+            </div>
+            <div class="row p-2" id="education">
+                <div class="col-4 col-md-4">
+                    <p>January 2015 - December 2019</p>
+                </div>
+                <div class="col-2 col-md-1">
+                    <span>-</span>
+                </div>
+                <div class="col-6 col-md-7">
+                    I attended Grassy Park high School and got my NQ4 form Grassy Park
+                    High School from which I matriculated with a Diploma pass
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <h3 class="py-3">Work Experience :</h3>
+            <div class="row p-2" id="education">
+                <div class="col-4 col-md-4">
+                    <p>September 2022 - Present</p>
+                </div>
+                <div class="col-2 col-md-1">
+                    <span>-</span>
+                </div>
+                <div class="col-6 col-md-7">
+                    <p>
+                        I am currently an Intern at LC Studio where I am upskilling myself as well as preparing myself for a
+                        job in a professional workforce
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <h3 class="py-3">Skills :</h3>
+            <div class="row" id="skills">
+                <div class="col">
+                    <h4 class="">Technical Skills</h4>
+                    <div class="row">
+                        <div class="col text-start">
+                            <div class="row">
+                                <div class="col">
+                                    <h6>Html</h6>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar"
+                                            style="width: 78%" aria-valuenow="70%" aria-valuemin="0" aria-valuemax="100">
+                                            <span>78%</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <h6>CSS</h6>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar"
+                                            style="width: 73%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
+                                            <span>73%</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <h6>Bootstrap</h6>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar"
+                                            style="width: 70%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
+                                            <span>70%</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <h6 class="p-1">mySQL</h6>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar"
+                                            style="width: 68%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
+                                            <span>68%</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <h6 class="p-1">JavaScript</h6>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar"
+                                            style="width: 57%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
+                                            <span>57%</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <h6 class="p-1">Vuex</h6>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar"
+                                            style="width: 56%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
+                                            <span>56%</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <h6 class="p-1">Node.js</h6>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar"
+                                            style="width: 40%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
+                                            <span>40%</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <h6 class="p-1">Express.js</h6>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar"
+                                            style="width: 37%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
+                                            <span>37%</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <h6 class="p-1">WordPress</h6>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar"
+                                            style="width: 70%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
+                                            <span>70%</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <h6 class="p-1">SCSS</h6>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar"
+                                            style="width: 32%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
+                                            <span>32%</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <h6 class="p-1">PHP</h6>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar"
+                                            style="width: 25%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
+                                            <span>25%</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col"></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- <div class="container">
-        <h3 class="py-3">Work Experience :</h3>
-
-    </div> -->
-    <div class="container">
-        <h3 class="py-3">Skills :</h3>
-        <div class="row" id="skills">
-            <div class="col">
-                <h4 class="">Technical Skills</h4>
-                <div class="row">
-                    <div class="col text-start">
-                        <div class="row">
-                            <div class="col">
-                                <h6>Html</h6>
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar" style="width: 78%" aria-valuenow="70%" aria-valuemin="0" aria-valuemax="100">
-                                        <span>78%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <h6>CSS</h6>
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar" style="width: 73%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
-                                        <span>73%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <h6>Bootstrap</h6>
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar" style="width: 70%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
-                                        <span>70%</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <h6 class="p-1">mySQL</h6>
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar" style="width: 68%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
-                                        <span>68%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <h6 class="p-1">JavaScript</h6>
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar" style="width: 57%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
-                                        <span>57%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <h6 class="p-1">Vuex</h6>
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar" style="width: 56%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
-                                        <span>56%</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <h6 class="p-1">Node.js</h6>
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar" style="width: 40%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
-                                        <span>40%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <h6 class="p-1">Express.js</h6>
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-striped rounded-pill" role="progressbar" style="width: 37%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
-                                        <span>37%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col">
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div class="row d-flex justify-content-center">
+            <h4 class="p-1">Soft Skills</h4>
+            <div class="col text-center">
+                <ul class="list-group">
+                    <li class="list-group-item m-1 p-2">Creativity</li>
+                    <li class="list-group-item m-1 p-2">Critical thinking</li>
+                    <li class="list-group-item m-1 p-2">Time management</li>
+                    <li class="list-group-item m-1 p-2">Communication</li>
+                </ul>
             </div>
         </div>
-    </div>
-    <div class="row d-flex justify-content-center">
-        <h4 class="p-1">Soft Skills</h4>
-        <div class="col text-center">
-            <ul class="list-group">
-                <li class="list-group-item m-1 p-2">Creativity</li>
-                <li class="list-group-item m-1 p-2">Critical thinking</li>
-                <li class="list-group-item m-1 p-2">Time management</li>
-                <li class="list-group-item m-1 p-2">Communication</li>
-            </ul>
-        </div>
-    </div>
-</section>
+    </section>
 </template>
 
 <script>
@@ -147,8 +191,8 @@
 
 <style scoped>
 #education {
-    height: 30vh;
-    padding: 20px;
+    height: 20vh;
+    padding: 10px;
     display: flex;
     justify-content: center;
     align-items: center;
